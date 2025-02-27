fd36b0f756d9f97423c3af0feb863ff2ee0c6aa9
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-02-28 00:26:26 +0530

    chore : removed unnecessary code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import RedirectPage from './pages/RedirectPage';
 
 function App() {
-  console.log('App component rendering'); // Add this for debugging
-  console.log(import.meta.env.SERVER_BASE_URL)
   return (
     <Router>
       <div className="min-h-screen bg-gray-100">
